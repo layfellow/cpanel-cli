44a78aabe2dbcb2a6d2abe4ca488b200122cca15
Marco Bravo Mejías <marco@layfellow.net>
2021-06-11 14:23:37 -0400

    Improve package build infrastructure
    - Move package information to `__init__.py`
    - Get rid of `setup.cfg` and move everything to `setup.py`
    - Move baseline Python version to 3.8 (as required by cpanel-api)

---
+++ cpanel/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Marco Bravo Mejías'
+__email__ = 'marco@layfellow.net'
+__version__ = '0.1.0'
+__copyright__ = 'Copyright 2021, ' + __author__
+__license__ = 'GPLv3'
+__description__ = 'cPanel 2 CLI'
+__url__ = 'https://github.com/layfellow/cpanel-cli'
