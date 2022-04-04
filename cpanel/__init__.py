ea9e9a84b66a54d7a7987a23152ccb1b1d837630
Marco Bravo Mejías <marco@layfellow.net>
2022-04-04 17:00:35 -0400

    Update copyright notice

--- cpanel/__init__.py
+++ cpanel/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Marco Bravo Mejías'
 __email__ = 'marco@layfellow.net'
 __version__ = '0.9.0'
-__copyright__ = 'Copyright 2021, ' + __author__
+__copyright__ = 'Copyright 2022, ' + __author__
 __license__ = 'GPLv3'
 __description__ = 'cPanel CLI'
 __url__ = 'https://github.com/layfellow/cpanel-cli'
