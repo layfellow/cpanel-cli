4d967c93b2ceb5a248293bb2bcf2a45b9901bb04
Marco Bravo Mejías <marco@layfellow.net>
2022-04-09 17:51:47 -0400

    Cosmetic documentation changes

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,10 @@
 import cpanel
+from sphinx.locale import get_translation
+
+_ = get_translation('contributing')
+
+def setup(app):
+	app.add_css_file('custom.css')
 
 project = cpanel.__description__
 copyright = cpanel.__copyright__
@@ -7,14 +13,19 @@ release = cpanel.__version__
 extensions = []
 templates_path = []
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
-html_static_path = []
-html_title = cpanel.__description__
+html_static_path = ['_static/']
+# html_title = ""
 locale_dirs = ['locale/']
 gettext_compact = False
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
 html_theme = 'sphinx_material'
 html_theme_options = {
+	'logo_icon': "&#x276F;&#x2581;",
+
 	# Set the name of the project to appear in the navigation.
-	'nav_title': cpanel.__description__,
+	'nav_title': _("cPanel CLI"),
 
 	# Set you GA account ID to enable tracking.
 	# 'google_analytics_account': 'UA-XXXXX',
@@ -23,17 +34,20 @@ html_theme_options = {
 	# 'base_url': 'https://project.github.io/project',
 
 	# Set the color and the accent color.
-	'color_primary': 'orange',
+	'color_primary': 'salmon',
 	'color_accent': 'teal',
 
 	# Set the repo location to get a badge with stats
 	'repo_url': cpanel.__url__,
-	'repo_name': cpanel.__description__,
+	# 'repo_name': cpanel.__description__,
+	'repo_name': "cpanel-cli",
 
 	# Visible levels of the global TOC; -1 means unlimited
-	'globaltoc_depth': 3,
+	'globaltoc_depth': 2,
 	# If False, expand all TOC entries
-	'globaltoc_collapse': False,
+	'globaltoc_collapse': True,
 	# If True, show hidden TOC entries
-	'globaltoc_includehidden': False
+	'globaltoc_includehidden': True,
+
+	'version_dropdown': True
 }
--- doc/index.rst
+++ doc/index.rst
@@ -66,3 +66,8 @@ For developers
    :maxdepth: 2
 
    contributing
+
+License
+=======
+
+`GNU General Public License v3 <https://www.gnu.org/licenses/gpl-3.0.en.html>`_
