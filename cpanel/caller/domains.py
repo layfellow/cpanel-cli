6a2a7d6229fa83e9d28542adee2b8ad0c53e833a
Marco Bravo Mejías <marco@layfellow.net>
2024-11-25 17:56:27 -0300

    Fix bug: functions was unreachable

--- cpanel/caller/domains.py
+++ cpanel/caller/domains.py
@@ -8,12 +8,12 @@ def call(host: CPanelEndpoint, cmd: str, args: List[str]) -> str:
 	r: str = ""
 	uapi: Api = host.client.uapi
 
-	if cmd_is(cmd, "list domain"):
-		r = host.dump(lambda: uapi.DomainInfo.list_domains())
-
-	elif cmd_is(cmd, "list domain data"):
+	if cmd_is(cmd, "list domain data"):
 		r = host.dump(lambda: uapi.DomainInfo.domains_data(
 			format = 'hash', return_https_redirects_status = 1))
+
+	elif cmd_is(cmd, "list domain"):
+		r = host.dump(lambda: uapi.DomainInfo.list_domains())
 
 	elif cmd_is(cmd, "get domain data"):
 		r = host.dump(lambda: uapi.DomainInfo.single_domain_data(
