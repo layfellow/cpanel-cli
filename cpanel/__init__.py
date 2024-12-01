5da4534a31f8e6bde807c3c08b0cfd052bc84722
Marco Bravo Mejías <marco@layfellow.net>
2024-12-01 20:52:46 -0300

    Bump version

--- cpanel/__init__.py
+++ cpanel/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Marco Bravo Mejías'
 __email__ = 'marco@layfellow.net'
-__version__ = '0.28'
+__version__ = '0.29'
 __copyright__ = '2022-2024, ' + __author__
 __license__ = 'GPLv3'
 __description__ = 'A command line interface for the cPanel Unrestricted API.'
